004a2c0a7f7f84416a4ba54bacb6151a715dba69
Tsiory <tsiory@kleap.co>
2022-03-24 09:43:06 +0300

    fix: handle language switch from url, change event barchart color

--- pages/share/[...id].js
+++ pages/share/[...id].js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Layout from 'components/layout/Layout';
 import WebsiteDetails from 'components/pages/WebsiteDetails';
 import useShareToken from 'hooks/useShareToken';
+import useLocale from 'hooks/useLocale';
 
 export default function SharePage() {
   const router = useRouter();
   const { id } = router.query;
   const shareId = id?.[0];
   const shareToken = useShareToken(shareId);
+
+  const { saveLocale } = useLocale();
+
+  const { lang } = router.query;
+
+  useEffect(() => {
+    if (lang) {
+      if (lang === 'en') {
+        saveLocale('en-US');
+      } else if (lang === 'fr') {
+        saveLocale('fr-FR');
+      }
+    }
+  }, [lang]);
 
   if (!shareToken) {
     return null;
